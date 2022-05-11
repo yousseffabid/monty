3d3d6ac8015498becf40b5c4cc7f6b79c6b1bd65
jebeniani <aymenjebeniani05@gmail.com>
2022-05-11 15:23:02 -0700

    monty

--- valid.c
+++ valid.c
@@ -1,17 +1,18 @@
 #include "monty.h"
 
 /**
- * valid - checks if th e syntax is valid
+ * valid - checks if the syntax is valid
  * @function_name: name of the function that can be "push"
  * @data: integer to be pushed if the function is push
  * Return: 0 (function is unknown) on failure and 1 on success
  */
 int valid(char *function_name)
 {
-	char name[][10] = {"push", "pall", "pint", "pop", "swap", "add", "nop"};
+	char name[][10] = {"push", "pall", "pint", "pop",
+		"swap", "add", "nop", "sub"};
 	unsigned int i;
 
-	for (i = 0; i < 7; i++)
+	for (i = 0; i < 8; i++)
 	{
 		if (strcmp(name[i], function_name) == 0)
 			return (1);
